b3a0d1f9d7babfab1011a4230a0724bfd4943862
Nawe1337 <155550180+Nawe1337@users.noreply.github.com>
2024-09-09 15:06:25 -0300

    trabajando en ejercicios de java

---
+++ Nahuel Francia/Java/New Folder/Aritmetica/src/Operaciones/Aritmetica.java
@@ -0,0 +1,14 @@
+
+package Operaciones;
+
+public class Aritmetica {
+    
+    int a;
+    int b;
+    
+    public void sumarNumeros(){
+        int resultado = a + b;
+        System.out.println(resultado);
+    }
+    
+}
