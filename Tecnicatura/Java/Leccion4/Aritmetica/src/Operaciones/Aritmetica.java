fe49e7dab7a6937341d1799d1671cb8c43a7770f
Gonzalo Juarez <gonzaloarieljuarez11@gmail.com>
2024-10-08 23:10:24 -0300

    Leccion 4 JAVA

--- Tecnicatura/Java/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
+++ Tecnicatura/Java/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -2,32 +2,41 @@
 package Operaciones;
 
 public class Aritmetica {
-    ////// atributos
+    // Atributos de la clase
+    // memoria heap, para atributos u objetos
     int a;
     int b;
     
     
-    ///// metodos 
+    // SOBRECARGA DE CONSTRUCTORES
+    // El constructor es un método especial
+    // CONSTRUCTOR 1
+    public Aritmetica(){ 
+        System.out.println("Se está ejecutando el constructor número 1");
+    }
+    // CONSTRUCTOR 2
+    public Aritmetica(int a, int b){ // no se usa var para atributos o parámetros
+        this.a = a;
+        this.b = b;
+        System.out.println("Se está ejecutando el constructor número 2");
+    }
+    
+    // METODO
     public void sumarNumeros(){
         int resultado = a + b;
-        System.out.println(resultado);
+        System.out.println("Resultado = " + resultado);
     }
     
     public int sumarConRetorno(){
-       int resultado;
-       resultado = a + b;
-        
-        return resultado;
+        //int resultado = a + b;
+        //return resultado;
+        return a + b;
     }
     
-    public int sumarConArgumentos(int arg1, int arg2){
-    a = arg1;
-    b = arg2;
-    
-    return sumarConRetorno();
-            
-        
+    public int sumarConArgumentos(int a, int b){
+        this.a = a; // el argumento a se asigna el atributo this.a
+        this.b = b; // el uso del this. es opcional
+        //return a + b;
+        return this.sumarConRetorno();
     }
-    
-    
 }
