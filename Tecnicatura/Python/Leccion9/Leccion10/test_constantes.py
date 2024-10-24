cd76690ffd4d5e1109d2ac74c5a55e1d6dfd4b63
GabrielBruni <166764577+GabrielBruni@users.noreply.github.com>
2024-10-24 00:31:59 -0300

    Subiendo lección9

---
+++ Tecnicatura/Python/Leccion9/Leccion10/test_constantes.py
@@ -0,0 +1,9 @@
+from Constantes import *
+from Constantes import Matematicas as Mate
+
+print(MI_CONSTANTE)
+#MI_CONSTANTE = 'Aquí está el nuevo valor para esta constante.'
+#print(MI_CONSTANTE) # Esto no se debe hacer!
+
+print(Mate)
+
