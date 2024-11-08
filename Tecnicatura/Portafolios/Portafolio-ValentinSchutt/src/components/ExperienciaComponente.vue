bf7ca50024b649476da4cd72984f40baec7e1037
SchuttValentin <schuttvalentin@gmail.com>
2024-11-08 11:19:19 -0300

    portfolio chichelon

---
+++ Tecnicatura/Portafolios/Portafolio-ValentinSchutt/src/components/ExperienciaComponente.vue
@@ -0,0 +1,103 @@
+<script setup>
+import icono1 from '/src/assets/development.svg';
+import icono2 from '/src/assets/testing.svg';
+import icono3 from '/src/assets/security.svg';
+import icono4 from '/src/assets/management.svg';
+import icono5 from '/src/assets/innovation.svg';
+
+import { ref } from 'vue';
+const titulo = 'Técnico Universitario en Programación';
+const fecha = 'Desde Enero 2023 / Noviembre 2024';
+const experiencias = ref([
+    { id: 1, src: icono1, titulo: 'Desarrollo de Aplicaciones Web y Móviles', parrafo: 'Creación de aplicaciones front-end y back-end, optimizadas para usabilidad y rendimiento. Utilizamos tecnologías modernas para construir interfaces intuitivas y sistemas robustos que satisfacen las necesidades de los usuarios.'},
+    { id: 2, src: icono2, titulo: 'Testing Manual y Automatizado', parrafo: 'Implementación de pruebas de calidad, tanto manuales como automatizadas, para garantizar el correcto funcionamiento y la experiencia de usuario en cada proyecto. Nos aseguramos de que nuestras aplicaciones sean fiables y de alto rendimiento.'},
+    { id: 3, src: icono3, titulo: 'Ciberseguridad y Protección de Datos', parrafo: 'Aplicación de estrategias y prácticas de ciberseguridad para proteger la integridad de nuestros sistemas. Contamos con un especialista en ciberseguridad que asegura que nuestros desarrollos cumplan con los más altos estándares de seguridad.'},
+    { id: 4, src: icono4, titulo: 'Gestión de Proyectos y Metodologías Ágiles', parrafo: 'Organización de proyectos en entornos colaborativos mediante metodologías ágiles como Scrum y Kanban. Este enfoque nos permite adaptarnos rápidamente a los cambios y optimizar el flujo de trabajo para entregas eficientes.'},
+    { id: 5, src: icono5, titulo: 'Innovación y Soluciones Personalizadas', parrafo: 'Nos enfocamos en ofrecer soluciones tecnológicas personalizadas que se adapten a las necesidades específicas de cada cliente. Nuestro proceso incluye investigación y adopción de tecnologías emergentes para mantenernos a la vanguardia del desarrollo de software.'},
+]);
+</script>
+
+<template>
+    <div class="card">
+        <h3 class="titulo">{{ titulo.toLocaleUpperCase() }}</h3>
+        <p class="fecha">{{ fecha }}</p>
+        <ul class="listado">
+            <li class="item" v-for="experencia in experiencias" :key="experencia.id">
+                <img class="imagen-svg" :src="experencia.src" width="55rem" :alt="experencia.titulo">
+                <p> {{ experencia.parrafo }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    display: flex;
+    flex-direction: column;
+    padding: 2rem;
+    background-color: rgb(28, 41, 52);
+    border-radius: 15px;
+}
+
+.titulo {
+    font-size: 1.5rem;
+    color: coral;
+}
+
+.fecha {
+    font-size: 1rem;
+    color: burlywood;
+    margin-bottom: 1rem;
+}
+
+.listado {
+    display: flex;
+    flex-direction: column;
+}
+
+.item {
+    align-items: center;
+    display: flex;
+    padding: 1rem;
+    gap: 1.5rem;
+}
+
+@media only screen and (max-width: 600px){
+
+    .card {
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+    background-color: rgb(28, 41, 52);
+    border-radius: 15px;
+}
+
+.titulo {
+    font-size: 16px;
+    color: coral;
+}
+
+.fecha {
+    font-size: 1rem;
+    color: burlywood;
+    margin-bottom: 1rem;
+}
+
+.listado {
+    display: flex;
+    flex-direction: column;
+}
+
+.item {
+    align-items: center;
+    display: grid;
+    padding: 10px 36px 10px 0px;
+    margin: 0 auto;
+    gap: 1.5rem;
+}
+
+
+
+}
+
+</style>
---
+++ Tecnicatura/Portafolios/Portafolio-ValentinSchutt/src/components/InteresesComponente.vue
@@ -0,0 +1,72 @@
+<script setup>
+import { ref } from 'vue';
+import FondoLava from './FondoLava.vue';
+//Este es un arreglo con ref para que se pueda reactivar el cambio de los intereses
+const intereses = ref([
+    'Innovación en el Desarrollo de Software: Estoy comprometido con la creación de soluciones innovadoras en el ámbito del desarrollo de software, abordando tanto aplicaciones web como móviles. Me apasiona explorar y aplicar nuevas tecnologías, técnicas y metodologías que no solo optimicen la eficiencia, sino que también mejoren la experiencia del usuario final.',
+'Inteligencia Artificial y Aprendizaje Automático: Interesado en el futuro de la tecnología, exploro el potencial de la inteligencia artificial para resolver problemas complejos. Dedico tiempo a aprender y aplicar técnicas de aprendizaje automático, con el objetivo de desarrollar proyectos que automaticen procesos y ofrezcan análisis de datos inteligentes.',
+'Ciberseguridad y Protección de Datos: Como desarrollador responsable, tengo un interés profundo en la ciberseguridad. Estoy comprometido con aprender e implementar prácticas de seguridad sólidas, protegiendo tanto los sistemas como los datos de los usuarios, y promoviendo la importancia de la seguridad en el desarrollo de software.',
+'Contribución a la Comunidad Tecnológica: Además de mi crecimiento profesional, busco contribuir activamente a la comunidad tecnológica. Me interesa compartir mis conocimientos a través de talleres, participar en eventos y colaborar en proyectos de código abierto, ayudando a construir un ecosistema inclusivo y colaborativo.'
+
+]);
+</script>
+
+<template>
+    <div class="intereses-contenedor">
+        <FondoLava />
+        <ul class="contenedor-lista">
+            <li class="item" v-for="interes in intereses" :key="interes">
+                {{ interes }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+.intereses-contenedor {
+    display: flex;
+    position: relative;
+    margin: 0 auto;
+    max-width: 85%;
+    padding: 1rem;
+}
+
+.contenedor-lista {
+    list-style-type: none;
+    padding: 1rem;
+    margin-bottom: .5rem;
+    color: var(--vt-c-white-soft);
+    font-size: 1.4rem;
+    text-shadow: 3px 3px 6px rgba(0, 0, 0, 1);
+}
+
+.item {
+    margin-bottom: 1rem;
+}
+
+
+@media only screen and (max-width: 600px){ 
+
+    .intereses-contenedor {
+    display: flex;
+    position: relative;
+    margin: 0 auto;
+    max-width: 85%;
+    max-height: 2700px;
+    padding: 0rem;
+}
+
+.contenedor-lista {
+    list-style-type: none;
+        padding: 18px;
+        margin-bottom: 2.5rem;
+        color: var(--vt-c-white-soft);
+        font-size: 16px;
+        text-shadow: 3px 3px 6px rgba(0, 0, 0, 1);
+}
+
+.item {
+    margin-bottom: 1rem;
+}
+}
+</style>
